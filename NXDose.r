764467e9ae1e3704a1a0da6b9d024b33946483cc
Eugene Mah <eugenemah@gmail.com>
2016-02-15 21:23:05 -0500

    Finally reading data in correctly

--- NXDose.r
+++ NXDose.r
@@ -4,6 +4,15 @@ require("ggplot2")
 
 setwd("C:/Users/Eugene Mah/Documents/workspace/NXDoseMonitoring")
 xmldata <- xmlParse("RT01NX01/DOSE_RT01NX01_20160121.xml")
-DoseData <- xmlToDataFrame(nodes=getNodeSet(xmldata,"//DoseStatistic"))[c("AgeGroup","ExamGroup","ExposureType","BodyPart","EI","DI")]
 
-DoseStat = ldply(xmlToList("RT01NX01/DOSE_RT01NX01_20160121.xml"),data.frame)
+AgeGroup=xmlToDataFrame(colClasses = c("character"),getNodeSet(xmldata,"//AgeGroup"))
+ExamGroup=xmlToDataFrame(colClasses=c("character"),getNodeSet(xmldata,"//ExamGroup"))
+ExposureType=xmlToDataFrame(colClasses=c("character"),getNodeSet(xmldata,"//ExposureType"))
+BodyPart=xmlToDataFrame(colClasses = c("character"),getNodeSet(xmldata,"//BodyPart"))
+EI=xmlToDataFrame(colClasses=c("numeric"),getNodeSet(xmldata,"//EI"))
+DI=xmlToDataFrame(colClasses=c("numeric"),getNodeSet(xmldata,"//DI"))
+TEI=xmlToDataFrame(colClasses=c("numeric"),getNodeSet(xmldata,"//TEI"))
+
+DoseData <- data.frame(AgeGroup,ExamGroup,ExposureType,BodyPart,EI,DI,TEI)
+DoseData <- rename(DoseData,c(text="AgeGroup",text.1="ExamGroup",text.2="ExposureType",text.3="BodyPart",text.4="EI",text.5="DI",text.6="TEI"))
+
